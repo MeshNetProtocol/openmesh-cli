646d5b5a4a14c83ebbde44fd93255b2871ca62ad
basdevelop <hyperorchid@outlook.com>
2026-01-08 12:07:20 +0800

    refactor design

---
+++ OpenMeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenMeshApp.swift
+//  OpenMesh
+//
+//  Created by wesley on 2026/1/8.
+//
+
+import SwiftUI
+
+@main
+struct OpenMeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
