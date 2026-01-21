be88e6922595b38e800f002108c35762133d0ff7
basdevelop <hyperorchid@outlook.com>
2026-01-21 20:33:58 +0800

    add app icon

--- openmesh-apple/OpenMeshMac/OpenMeshMacApp.swift
+++ openmesh-apple/OpenMeshMac/OpenMeshMacApp.swift
@@ -18,7 +18,7 @@ struct openmeshApp: App {
 
     var body: some Scene {
         // 2. 使用 MenuBarExtra 代替 WindowGroup
-        MenuBarExtra("OpenMesh", systemImage: "network") {
+        MenuBarExtra {
             // 3. 这里编写点击菜单后显示的菜单项
             VStack(spacing: 10) {
                 Text("OpenMesh VPN")
@@ -46,6 +46,21 @@ struct openmeshApp: App {
                 .keyboardShortcut("q") // 快捷键 Cmd+Q
             }
             .padding()
+        } label: {
+            Label {
+                Text("OpenMesh")
+            } icon: {
+                statusBarIcon
+                    .renderingMode(.original)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 18, height: 18)
+            }
+            .labelStyle(.iconOnly)
         }
+    }
+
+    private var statusBarIcon: Image {
+        Image(vpnManager.isConnected ? "mesh_on" : "mesh_off")
     }
 }
